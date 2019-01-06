7fbc5110f10e243dd3c2508994e35dde2ab4f533
José L. S. A <alagunasalahaddin@live.com>
2019-01-06 12:14:42 +0300

    [reviewjs] :sunny: template strings.

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,2 +1,12 @@
 const name = 'José Luis';
 const job = 'Web developer';
+
+console.log(`Name: ${name}, Job: ${job}`);
+
+const contentApp = document.querySelector('#app');
+let html = `<ul>
+<li>Name: ${name}</li>
+<li>Job: ${job}</li>
+</ul>
+`;
+contentApp.innerHTML = html;
