c7f4e9d8c86501fa07438b3a98ee348465830553
José L. S. A <alagunasalahaddin@live.com>
2019-01-22 00:01:13 +0300

    [reviewjs] :sunny: Inheriting classes.

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -261,11 +261,23 @@ class Task{
     }
 }
 
+class PendingPurchase extends Task {
+    constructor(title, priority, quantity){
+        super(title, priority);
+        this.quantity = quantity;
+    }
+    show(){
+        return `${super.show()}, Quantity: ${this.quantity}`;
+    }
+}
+
 // creating objects
 let task1 = new Task('Learning JavaScript', 'High');
 let task2 = new Task('Make Tea', 'Low');
+let purchase1 = new PendingPurchase('Soap', 'Urgent', 3);
 
 console.log('====================================');
 console.log(task1.show());
 console.log(task2.show());
+console.log(purchase1.show());
 console.log('====================================');
