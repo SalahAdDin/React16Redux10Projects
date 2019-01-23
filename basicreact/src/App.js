c86f964b89e8639b59a29e519a695bf5d59494b1
José L. S. A <alagunasalahaddin@live.com>
2019-01-23 20:32:37 +0300

    [basicreact] :sunny: React JSX.

--- basicreact/src/App.js
+++ basicreact/src/App.js
@@ -4,15 +4,31 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const comment = `Inside of JSX {} execute JavaScript code`;
+    const comment2 = `
+      <React.Fragment> can be used as a parent, 
+      it does not render any html tag; useful for flex-box.
+    `;
     return (
-      React.createElement(
-        'div',
-        null,
-        React.createElement(
-          'input',
-          {type: "text", value: "Value"},
-        )
-      )
+      // RETURN must to return only one element: 
+      //  multiple elements must be inside of a parent
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {comment}
+        </header>
+      </div>
     );
   }
 }
