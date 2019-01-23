11d66c128f2aaa68ed48961c1bc5e87566437e03
José L. S. A <alagunasalahaddin@live.com>
2019-01-23 20:42:51 +0300

    [basicreact] :sunny: Creating a first component.

---
+++ basicreact/src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FirstComponent extends Component{
+    render(){
+        return(
+            <div>
+                <p>First component Hello!!!</p>
+            </div>
+        )
+    }
+}
+
+export default FirstComponent;
--- basicreact/src/index.js
+++ basicreact/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+//import App from './App';
+import FirstComponent from './components/FirstComponent'
 import * as serviceWorker from './serviceWorker';
 
 // React no JSX
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FirstComponent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
