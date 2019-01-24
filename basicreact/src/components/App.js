6676e7561ee7ea3598de92f5ae2920cb915393af
José L. S. A <alagunasalahaddin@live.com>
2019-01-24 23:18:25 +0300

    [basicreact] :sunny: Properties.

--- basicreact/src/components/App.js
+++ basicreact/src/components/App.js
@@ -4,11 +4,21 @@ import Footer from './Footer';
 import Header from './Header';
 
 class App extends Component{
+
     render(){
+
+        const products=[
+            {name: 'Book', cost: 200},
+            {name: 'Music CD', cost: 100},
+            {name: 'Music Instrument', cost: 800},
+            {name: 'Music Player', cost: 1500},
+            {name: 'Album Special Ed.', cost: 500},
+        ]    
+
         return(
             <div>
-                <Header />
-                <Products />
+                <Header title="Mi nueva Tienda Virtual"/>
+                <Products products={products}/>
                 <Footer />
             </div>
         )
--- basicreact/src/components/Header.js
+++ basicreact/src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header>
-            <h1>Mi nueva Tienda Virtual</h1>
+            <h1>{props.title}</h1>
         </header>
     )
 }
