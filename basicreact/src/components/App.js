ba6300a606e812ec968bbbca3c11dded13e9f2ed
José L. S. A <alagunasalahaddin@live.com>
2019-01-24 23:56:28 +0300

    [basicreact] :sunny: State and lifecycle methods.

--- basicreact/src/components/App.js
+++ basicreact/src/components/App.js
@@ -5,20 +5,54 @@ import Header from './Header';
 
 class App extends Component{
 
-    render(){
+    // With constructor
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         products : [
+    //             {name: 'Book', cost: 200},
+    //             {name: 'Music CD', cost: 100},
+    //             {name: 'Music Instrument', cost: 800},
+    //             {name: 'Music Player', cost: 1500},
+    //             {name: 'Album Special Ed.', cost: 500},
+    //         ]    
+    //     }; 
+    // }
 
-        const products=[
+    // Without constructor
+    state = {
+        products : [
+            // {name: 'Book', cost: 200},
+            // {name: 'Music CD', cost: 100},
+            // {name: 'Music Instrument', cost: 800},
+            // {name: 'Music Player', cost: 1500},
+            // {name: 'Album Special Ed.', cost: 500},
+        ]    
+    }
+
+    componentDidMount(){
+
+        const products = [
             {name: 'Book', cost: 200},
             {name: 'Music CD', cost: 100},
             {name: 'Music Instrument', cost: 800},
             {name: 'Music Player', cost: 1500},
-            {name: 'Album Special Ed.', cost: 500},
-        ]    
+            {name: 'Album Special Ed.', cost: 500}
+        ] 
+
+        setTimeout( () => {
+            this.setState({   
+                products : products  
+            });
+        }, 3000);
+    }
+
+    render(){
 
         return(
             <div>
                 <Header title="Mi nueva Tienda Virtual"/>
-                <Products products={products}/>
+                <Products products={this.state.products}/>
                 <Footer />
             </div>
         )
