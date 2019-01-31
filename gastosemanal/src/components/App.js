1cb3ce0fab12e616e4e3d5a42ba8d54c7233aa5b
José L. S. A <alagunasalahaddin@live.com>
2019-01-31 18:57:27 +0300

    [gastosemana] :sunny: refactaring, adding initial header and containers.

---
+++ gastosemanal/src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../css/App.css';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+      <Header title='Gasto Semanal'/>
+        <div className="contenido-principal contenido">
+          <div className="row">
+            <div className="one-half column"></div>
+            <div className="one-half column"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
