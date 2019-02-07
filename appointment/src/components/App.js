e6b8afdf93e0f0d835cfd37fc9fbbb1c8aa0e3f0
José L. S. A <alagunasalahaddin@live.com>
2019-02-07 19:29:18 +0300

    [appoinment] :sunny: Created the Appointment component.

--- appointment/src/components/App.js
+++ appointment/src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import Header from './Header';
+import Appointment from './appointment';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header title={'Administrador de Pacientes de Veterinaria'}/>
+        <div className="row">
+          <div className="col-md-6">
+            <Appointment />
+          </div>
+        </div>
       </div>
     );
   }
