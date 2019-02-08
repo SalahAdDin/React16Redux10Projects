436e507cad3447d5c4fca8bc4c054dab5a68b707
José L. S. A <alagunasalahaddin@live.com>
2019-02-08 09:34:43 +0300

    [appoinment] :sunny: Reading form's values.

--- appointment/src/components/Appointment.js
+++ appointment/src/components/Appointment.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class Appointment extends Component {
+
+    // refs
+    namePetRef = React.createRef();
+    nameOwnerRef = React.createRef();
+    dateRef = React.createRef();
+    timeRef = React.createRef();
+    symptomsRef = React.createRef();
+
     state = {
 
     }
@@ -15,36 +23,36 @@ class Appointment extends Component {
             <div className="card mt-5">
                 <div className="card-body">
                     <h2 className="card-title text-center mb-5">Agrega las Citas aquí:</h2>
-                    <form onSubmit={}>
+                    <form onSubmit={this.createNewAppointment}>
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-3 col-form-label">Nombre Mascota</label>
                             <div className="col-sm-8 col-lg-9">
-                                <input type="text" className="form-control" placeholder="Nombre Mascota" />
+                                <input ref={this.namePetRef} type="text" className="form-control" placeholder="Nombre Mascota" />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-3 col-form-label">Nombre Dueño</label>
                             <div className="col-sm-8 col-lg-9">
-                                <input type="text" className="form-control"  placeholder="Nombre Dueño de la Mascota" />
+                                <input ref={this.nameOwnerRef} type="text" className="form-control"  placeholder="Nombre Dueño de la Mascota" />
                             </div>
                         </div>
 
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
                             <div className="col-sm-8 col-lg-4  mb-4 mb-lg-0">
-                                <input type="date" className="form-control" />
+                                <input ref={this.dateRef} type="date" className="form-control" />
                             </div>                            
 
                             <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
                             <div className="col-sm-8 col-lg-4">
-                                <input type="time" className="form-control" />
+                                <input ref={this.timeRef} type="time" className="form-control" />
                             </div>
                         </div>
 
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-3 col-form-label">Sintomas</label>
                             <div className="col-sm-8 col-lg-9">
-                                <textarea  className="form-control"></textarea>
+                                <textarea ref={this.symptomsRef} className="form-control"></textarea>
                             </div>
                         </div>
                         <div className="form-group row justify-content-end">
